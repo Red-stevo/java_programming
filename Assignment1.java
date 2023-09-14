35cd6e6a81435a8bb836c0e590bd1d41e356d2f4
redsteve <redsteve@localhost.localdomain>
2023-09-14 16:46:06 +0300

    Pushing java Assignment1.

---
+++ Assignment1.java
@@ -0,0 +1,35 @@
+
+class table
+{
+    public static void main(String a[])
+    {
+        System.out.println("*************************Adamson Computers Ltd*************************");
+        System.out.println("#99 ACS Building Gilmore Ave. New Mla.. Quezon City * Tel. No:123-4567");
+        System.out.println("\t\t VAT REG. TIN 098-765-4321-00_00");
+        System.out.println("\t\t\t SALES INVOICE");
+        System.out.println();
+        System.out.println();
+        System.out.println("Sold to:Jim Kim\t\t\t\t\t Date:28/04/2020");
+        System.out.println("Address:private bag\t\t\t\t Contact Number:0100100100");
+        System.out.println();
+        System.out.println();
+        System.out.println("============================================================================");
+        System.out.println("Qty\t | \t Item Description\t | \t Unit Price \t |Amount");
+        System.out.println("============================================================================");
+        System.out.println("4\t | \t Pencil\t\t\t | \t 20 \t\t |80.00");
+        System.out.println("----------------------------------------------------------------------------");
+        System.out.println("10\t | \t Duster\t\t\t | \t 50\t\t |500.00");
+        System.out.println("----------------------------------------------------------------------------");
+        System.out.println("9\t | \t Pens \t\t\t | \t 30\t\t |270.00");
+        System.out.println("----------------------------------------------------------------------------");
+        System.out.println("10\t | \t crayon \t\t | \t 80\t\t |800.00");
+
+        System.out.println("============================================================================");
+        System.out.println("\t\t\t\t\t |Sub Total\t\t |1650.00");
+        System.out.println("\t\t\t\t\t ===================================");
+        System.out.println("\t\t\t\t\t |VAT(16%) \t\t |264.00");
+        System.out.println("\t\t\t\t\t ===================================");
+        System.out.println("\t\t\t\t\t |Total Amount\t\t |1914.00");
+        System.out.println("\t\t\t\t\t ===================================");
+    }
+}
